c909d5d30435c7649de3e3d5a2dec98ba45090a4
StefVuck <stefanvuck@gmail.com>
2023-11-01 18:06:26 +0000

    Started on project, potentially non functional at the moment

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {HomeScreen} from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from "./store";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import HomeScreen from './screens/HomeScreen';
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView>
+
+
+          <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headershown: false,}} /> 
+
+
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
